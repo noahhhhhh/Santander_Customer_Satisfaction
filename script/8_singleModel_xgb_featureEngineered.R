df2529bdfca7ad95e26e8f72995f30906a83868e
noahhhhhh <noah.xm.shaw@gmail.com>
2016-04-02 23:05:53 +1100

    tuning xgb with vars

--- script/8_singleModel_xgb_featureEngineered.R
+++ script/8_singleModel_xgb_featureEngineered.R
@@ -76,48 +76,48 @@ params <- list(booster = "gbtree"
                , nthread = 8
                , objective = "binary:logistic"
                , eval_metric = "auc"
-               , max_depth = 7 # 5
-               , subsample = .74 #.74
-               # , min_child_weight = 1
-               , colsample_bytree = .7 #.7
-               , eta = .02 #.02
+               , max_depth = 6 # 5; 4:0.850155 5:0.8504593 6:0.8507164 7:0.850147l 9:0.8444527 
+               , subsample = .74 #.74; .65:0.85016 .70: 0.8502957; .73:0.8511474 .74:0.8514069 .75:0.8508004 .8: 0.850362
+               , min_child_weight = 1 # 1; 2:0.8428779
+               , colsample_bytree = .6 #.7; .5:0.8494549 .62:0.8504506 .6:0.8514069* .65:0.8504949 .8: 0.8506108
+               , eta = .022 #.02; .021:0.8510815 .022:0.8513952* .023:0.8512173 .025:0.850257
                )
-# round <- 10
-# ls.pred.valid <- list()
-# ls.pred.test <- list()
-# for(i in 1:round){
-#     set.seed(1234 * i)
-#     md.xgb <- xgb.train(params = params
-#                         , data = dmx.train
-#                         , nrounds = 100000
-#                         , early.stop.round = 50
-#                         , watchlist = watchlist
-#                         , print.every.n = 50
-#                         , verbose = F
-#     )
-#     # valid
-#     pred.valid <- predict(md.xgb, dmx.valid)
-#     ls.pred.valid[[i]] <- pred.valid
-#     print(paste("round:", i, "valid auc:", auc(dt.valid$TARGET, pred.valid)))
-# 
-#     # test
-#     pred.test <- predict(md.xgb, x.test)
-#     ls.pred.test[[i]] <- pred.test
-# }
-# pred.valid.mean <- apply(as.data.table(sapply(ls.pred.valid, print)), 1, mean)
-# auc(dt.valid$TARGET, pred.valid.mean)
-set.seed(1234)
-md.xgb <- xgb.train(params = params
-                    , data = dmx.train
-                    , nrounds = 100000
-                    , early.stop.round = 50
-                    , watchlist = watchlist
-                    , print.every.n = 50
-                    , verbose = F
-)
-# valid
-pred.valid <- predict(md.xgb, dmx.valid)
-auc(dt.valid$TARGET, pred.valid)
+round <- 10
+ls.pred.valid <- list()
+ls.pred.test <- list()
+for(i in 1:round){
+    set.seed(1234 * i)
+    md.xgb <- xgb.train(params = params
+                        , data = dmx.train
+                        , nrounds = 100000
+                        , early.stop.round = 50
+                        , watchlist = watchlist
+                        , print.every.n = 50
+                        , verbose = F
+    )
+    # valid
+    pred.valid <- predict(md.xgb, dmx.valid)
+    ls.pred.valid[[i]] <- pred.valid
+    print(paste("round:", i, "valid auc:", auc(dt.valid$TARGET, pred.valid)))
+
+    # test
+    pred.test <- predict(md.xgb, x.test)
+    ls.pred.test[[i]] <- pred.test
+}
+pred.valid.mean <- apply(as.data.table(sapply(ls.pred.valid, print)), 1, mean)
+auc(dt.valid$TARGET, pred.valid.mean)
+# set.seed(1234)
+# md.xgb <- xgb.train(params = params
+#                     , data = dmx.train
+#                     , nrounds = 100000
+#                     , early.stop.round = 50
+#                     , watchlist = watchlist
+#                     , print.every.n = 50
+#                     , verbose = F
+# )
+# # valid
+# pred.valid <- predict(md.xgb, dmx.valid)
+# auc(dt.valid$TARGET, pred.valid)
 # 0.8447578 73 train vs valid 
 # 0.8449555 73 train vs valid with cnt0
 # 0.8458973 73 train vs valid with cnt0, tuned(incorrect)
@@ -138,6 +138,9 @@ auc(dt.valid$TARGET, pred.valid)
 # 0.8492684 73 train vs valid with 10 rounds of mean of xgb, with cnt0, cnt1, kmeans, knn, with bench tuning and .74 ss
 # 0.8490421 73 train vs valid with 10 rounds of mean of xgb, with cnt0, cnt1, kmeans, knncombined, with bench tuning and .74 ss
 # 0.8503138 73 train vs valid with 10 rounds of mean of xgb, with cnt0, cnt1, kmeans, vars, with bench tuning and .74 ss
+# 0.8514069 73 train vs valid with single xgb with cnt0, cnt1, kmeans, vars with my tuning
+# 0.8509671 73 train vs valid with 10 xgb with cnt0, cnt1, kmeans, vars with my tuning
+
 ## importance
 importance <- xgb.importance(setdiff(names(dt.train), c("ID", "TARGET")), model = md.xgb)
 importance[Feature == "cnt0"]
@@ -157,7 +160,7 @@ as.data.frame(importance) # cnt1 top 4, cnt0 top 8, kmeans top 106
 pred.test.mean <- apply(as.data.table(sapply(ls.pred.test, print)), 1, mean)
 # submit <- data.table(ID = dt.test$ID, TARGET = pred.test)
 submit <- data.table(ID = dt.test$ID, TARGET = pred.test.mean)
-write.csv(submit, file = "submission/21_10_xgb_73_train_valid_cnt0_cnt1_kmeans_vars_benchmark_tuning_74_ss.csv", row.names = F)
+write.csv(submit, file = "submission/23_10_xgb_73_train_valid_cnt0_cnt1_kmeans_vars_my_tuning.csv", row.names = F)
 # 0.836426 73 train vs valid 
 # 0.836738 73 train vs valid with cnt0
 # 0.837194 73 train vs valid with cnt0, tuned(incorrect)
@@ -178,3 +181,5 @@ write.csv(submit, file = "submission/21_10_xgb_73_train_valid_cnt0_cnt1_kmeans_v
 # 0.839139 73 train vs valid with 10 rounds of mean of xgb, with cnt0, cnt1, kmeans, knn, with bench tuning and .74 ss
 # 0.838047 73 train vs valid with 10 rounds of mean of xgb, with cnt0, cnt1, kmeans, knncombined, with bench tuning and .74 ss
 # 0.839522 73 train vs valid with 10 rounds of mean of xgb, with cnt0, cnt1, kmeans, vars, with bench tuning and .74 ss
+# 0.839537 73 train vs valid with single xgb with cnt0, cnt1, kmeans, vars with my tuning
+# 0.839343 73 train vs valid with 10 xgb with cnt0, cnt1, kmeans, vars with my tuning
